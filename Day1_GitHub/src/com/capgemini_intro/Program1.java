24243532e2b7b23a3fdf8feb55bcea6a82f19678
Admin <Admin@DELL>
2022-07-13 16:17:13 +0530

    first commit

--- Day1_GitHub/src/com/capgemini_intro/Program1.java
+++ Day1_GitHub/src/com/capgemini_intro/Program1.java
@@ -3,7 +3,7 @@ package com.capgemini_intro;
 public class Program1 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to M21_C2TC Day 1 Program");
+		System.out.println("***Welcome to M21_C2TC Day 1 Program***");
 
 	}
 
